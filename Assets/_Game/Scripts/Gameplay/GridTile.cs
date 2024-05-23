b0c96c8631794804d4cd468df1a48a15420b610c
Pham Viet Hoang <105777915+phamhoanggg@users.noreply.github.com>
2024-05-23 11:20:12 +0700

    add A star

--- Assets/_Game/Scripts/Gameplay/GridTile.cs
+++ Assets/_Game/Scripts/Gameplay/GridTile.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class GridTile : MonoBehaviour
 {
+    private Vector2Int coordinate;
+    public Vector2Int Coordinate => coordinate;
 
+    public LayerMask constructionLayer;
+
+    public void OnInit(int x, int y)
+    {
+        SetCoord(x, y);
+
+    }
+
+    public void SetCoord(int x, int y)
+    {
+        coordinate = new Vector2Int(x, y);
+    }
+
+    public bool IsWalkable()
+    {
+        Collider2D col = Physics2D.OverlapCircle(transform.position, 0.1f, constructionLayer);
+        return col == null;
+    }
 }
--- Assets/_Game/Scripts/Manager/GridTileManager.cs
+++ Assets/_Game/Scripts/Manager/GridTileManager.cs
@@ -6,16 +6,29 @@ public class GridTileManager : SingletonMB<GridTileManager>
 {
     [SerializeField] private GridTile prefabTile;
     [SerializeField] Transform tileParent;
+    private GridTile[,] tile_array;
+    private bool[,] walkableMap;
 
     public void SpawnAllTiles(int width, int height)
     {
+        tile_array = new GridTile[width, height];
+        walkableMap = new bool[width, height];
+
         for (int y = 0; y < height; y++)
         {
             for(int x = 0; x < width; x++)
             {
                 GridTile newTile = Instantiate(prefabTile, new Vector3(x/2f, y/2f, 0), Quaternion.identity, tileParent);
                 newTile.name = "TILE (" + x + "," + y + ")";
+                tile_array[x, y] = newTile;
+                newTile.SetCoord(x, y);
+                walkableMap[x, y] = newTile.IsWalkable();
             }
         }
+    }
+
+    public bool[,] GetWalkableMap()
+    {
+        return walkableMap;
     }
 }
